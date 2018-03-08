ea74980262b5ad573366184fd38a9177ffd54824
Edgar <fjalex@example.com>
2018-03-08 23:38:16 +0800

    v1.3.1  显示文章标题 v1.3.2 查看文章内容

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.blog_title, name='blog_title'),
+    url(r'^(?P<article_id>\d+)/$', views.blog_article, name='blog_article')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import BlogArticles
 
@@ -7,3 +7,8 @@ def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request, "blog/titles.html", {"blogs": blogs})
 
+def blog_article(request, article_id):
+    #article = BlogArticles.objects.get(id=article_id)
+    article = get_object_or_404(BlogArticles, id=article_id)
+    pub = article.publish
+    return render(request, 'blog/content.html', {"article": article, "publish": pub})

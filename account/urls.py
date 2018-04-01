76b7439dadc53807119d12da4d27513a327d4197
Edgar <fjalex@example.com>
2018-04-01 11:26:39 +0800

    2.4.1 end 简单注册

--- account/urls.py
+++ account/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import url
 from django.conf import settings
-
-from django.conf import settings
-
 from django.contrib.auth import views as auth_views
 
-#from . import views
+from . import views
 
 urlpatterns = [
     #url(r'^login/$', views.user_login, name='user_login'), #自定义的登录
     url(r'^login/$', auth_views.login, name='user_login'),  #django内置的登录
     url(r'^new-login/$', auth_views.login, {"template_name": "account/login.html", }),
     url(r'^logout/$', auth_views.logout, {'template_name': "account/logout.html"}, name='user_logout'),
+    url(r'^register/$', views.register, name="user_register"),
 ]
